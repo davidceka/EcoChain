25f6499050a9dd35a65f7695b67ae29f824b73d4
David Ceka <dav.ceka@gmail.com>
2022-01-02 15:12:26 +0100

    Merge branch 'main' of https://github.com/davidceka/EcoChain into main

--- app.js
+++ app.js
@@ -1,21 +1,36 @@
-var path = require('path');
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
+const path = require('path');
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const authRouter=require('./routes/auth')
+const pagesRouter=require('./routes/pages')
+const dotenv=require('dotenv')
+const session=require('express-session')
+const crypto=require('crypto')
 
-var handlebars = require('express-handlebars').create({
+const handlebars = require('express-handlebars').create({
   layoutsDir: path.join(__dirname, "views/layouts"),
   partialsDir: path.join(__dirname, "views/partials"),
   defaultLayout: 'layout',
   extname: 'hbs'
 });
 
-app.engine('hbs', handlebars.engine);
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, "views"));
+dotenv.config({
+  path:'./configs/.env'
+})
 
-app.use(require('./routes/pages'));
-app.use(require('./routes/auth'))
+
+app.engine('hbs', handlebars.engine)
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, "views"))
+
+
+var secret = crypto.randomBytes(32).toString('hex');
+console.log(secret)
+
+app.use(pagesRouter)
+app.use(authRouter)
+
 
 app.listen(5000, ()=>{
     console.log('Server is listening on port 5000')
