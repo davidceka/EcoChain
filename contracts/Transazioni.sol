c5a821e9ef9fa6024a0a50b5c1b6e894ca62480c
David Ceka <dav.ceka@gmail.com>
2022-01-12 22:06:45 +0100

    fix transazioni

--- contracts/Transazioni.sol
+++ contracts/Transazioni.sol
@@ -2,21 +2,34 @@
 pragma solidity ^0.8.2;
 
 import "./CarbonFootprint.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract Transazione {
     CarbonFootprint tokenContract;
+    uint256 materiaPrimaCounter;
+    uint256 prodottiCounter;
+
+    uint256 prodottiId;
+    uint256 materiePrimeId;
 
     constructor (address _carbonFootprintContract){
         tokenContract=CarbonFootprint(_carbonFootprintContract);
     }
 
-
+    function incrementMaterie() internal{
+        materiePrimeId++;
+    }
+    function incrementProdotti() internal{
+        prodottiId++;
+    }
 
     struct MateriaPrima{
         uint256 id_lottomateria;
         string nome;
         address owner;
-        uint256 token;    
+        uint256 token;  
+        uint256 amount;  
+        bool not_available;
     }
 
 
@@ -25,22 +38,44 @@ contract Transazione {
         uint256 id_prodotto;
         string nome;
         address owner;
+        uint256 token;
 
     }
     mapping(uint256=>MateriaPrima) public materia_prima;
     mapping(uint256=>Prodotti) public prodotti;
 
-    function creaToken()  public{
+    function creaNuovaMateriaPrima(string memory _nome,uint256 _amount) public{
         uint256 tokenId=tokenContract.safeMint(msg.sender);
         materia_prima[0]=MateriaPrima({
-            id_lottomateria:0,
-            nome:"pomodori",
+            id_lottomateria:materiePrimeId,
+            nome:_nome,
+            owner:msg.sender,
+            token:tokenId,
+            amount:_amount
+        });
+        materiaPrimaCounter++;
+        incrementMaterie();
+    }
+    function creaNuovoProdotto(string memory _nome,uint256 _lotto) public{
+
+        // da aggiungere: ricavare l'impatto dal vecchio token e sommarlo al nuovo
+        uint256 tokenId=tokenContract.safeMint(msg.sender);
+        prodotti[prodottiCounter]=Prodotti({
+            id_prodotto:prodottiId,
+            nome:_nome,
             owner:msg.sender,
             token:tokenId
         });
-        
+        prodottiCounter++;
+        incrementProdotti();
+        materia_prima[_lotto].not_available=true;
+        materiaPrimaCounter--;
     }
-    function getToken() view public returns (MateriaPrima memory){
+
+    function getMateriaPrima() view public returns (MateriaPrima memory){
+        return materia_prima[0];
+    }
+    function getProdotto() view public returns (MateriaPrima memory){
         return materia_prima[0];
     }
     
